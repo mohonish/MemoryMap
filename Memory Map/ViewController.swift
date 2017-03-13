0a3253234259bb010d9ad9560845ef258d492514
Mohonish Chakraborty <mohonish.c@gmail.com>
2017-03-13 12:53:26 +0530

    initial commit. set up project

---
+++ Memory Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memory Map
+//
+//  Created by Mohonish Chakraborty on 13/03/17.
+//  Copyright © 2017 mohonish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
